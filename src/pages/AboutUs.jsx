ac43a566ab222d59190eaebebdfe5ef7a5811b24
kayla <kayladatte@gmail.com>
2024-10-06 19:03:42 -0700

    added content to the About Us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,59 @@
-
-
-export default function AboutUs(){
-    return(
-        <p>this is the about us page</p>
-    );
+export default function AboutUs() {
+  return (
+    <div>
+      <div>
+        <h2>About Us</h2>
+        <p>
+          This is a paragraph about PowPAC. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <h3>Board Members</h3>
+        <figure>
+          <img src="../assets/David_Dartt.jpg"></img>
+          <figcaption>
+            President<br></br>David Dartt
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="../assets/SteveMurdock.png"></img>
+          <figcaption>
+            Vice President<br></br>Steven Murdock
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="../assets/Joye_Davisson.jpg"></img>
+          <figcaption>
+            Secretary<br></br>Joye Davisson
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="../assets/WalkenhorstED1.jpg"></img>
+          <figcaption>
+            Treasurer<br></br>Kari Seagren Walkenborst
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  );
 }
